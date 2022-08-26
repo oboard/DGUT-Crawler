23af1d43eb95a9785ea4e2bed9c9d7f1eecd4c58
oboard <oboard@outlook.com>
2022-08-26 22:11:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+DGUT 信息爬虫
+python 3.9.10
+mysql 8.0.29
+
+person.py:使用 ‘http://lgapp.dgut.edu.cn/api/home/getPersonDetail’ api获取莞工通讯录信息
---
+++ person.py
@@ -0,0 +1,36 @@
+import requests
+import pymysql
+import json
+import time
+# 打开数据库连接
+db = pymysql.connect(host='127.0.0.1',
+                     user='root', password='luoyuhang123.', database='dgut')
+
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+
+# 使用execute方法执行SQL语句
+cursor.execute('SELECT VERSION()')
+sql = 'select * from Student'
+for i in range(475029):
+    kw = {'uid': i+1011}
+    # 设置请求头
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ6ZW1jaG8iLCJhdWQiOiIiLCJleHAiOjE2NjE1Mjc1MTcsImRhdGEiOnsidWlkIjoxMTE4NTY3N319.lPCIFj8MjtDGvXURU730lXBiW4nWpfMlsVr8k7vQNdc"
+    }
+    # params 接收一个字典或者字符串的查询参数，字典类型自动转换为url编码，不需要urlencode()
+    response = requests.get(
+        "http://lgapp.dgut.edu.cn/api/home/getPersonDetail", params=kw, headers=headers)
+    person = json.loads(response.text)['info']
+    if (person == []):
+        continue
+    print(person['uid'])
+    sql1 = f"insert into person values ('{person['uid']}','{person['type']}','{person['stage']}','{person['sex']}','{person['name']}','{person['username']}','{person['phone']}','{person['short_phone']}','{person['office_phone']}','{person['qq']}','{person['wechat']}','{person['email']}','{person['faculty_name']}','{person['faculty_id']}','{person['class_name']}','{person['class_id']}','{person['address']}','{person['avatar']}')"
+    cursor.execute(sql1)
+    # 执行sql语句
+    db.commit()
+    time.sleep(0.01)
+cursor.close()
+db.close()
