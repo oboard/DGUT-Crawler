1942f49cdbd664b5c4731fe359b33ddf2536cd38
oboard <oboard@outlook.com>
2022-08-26 22:21:22 +0800

    token

--- person.py
+++ person.py
@@ -19,7 +19,7 @@ for i in range(475029):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": f"{token}"
+        "Authorization": token
     }
     # params 接收一个字典或者字符串的查询参数，字典类型自动转换为url编码，不需要urlencode()
     response = requests.get(
